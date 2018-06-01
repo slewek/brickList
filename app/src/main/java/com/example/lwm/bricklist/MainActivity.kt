9684fddc2a1ba4fa4ce93482fe9c160fb04e0534
slewek <slewek13@gmail.com>
2018-06-01 13:48:14 +0200

    Adding view of new activities

--- app/src/main/java/com/example/lwm/bricklist/MainActivity.kt
+++ app/src/main/java/com/example/lwm/bricklist/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.example.lwm.bricklist
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
+
+    var REQUEST_CODE = 1000
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun addOnClick(v : View){
+        val i = Intent(this, AddProjectActivity::class.java)
+        startActivityForResult(i,REQUEST_CODE)
+    }
+
+    fun settingsOnClick(v : View){
+        val i = Intent(this,SettingActivity::class.java)
+        startActivityForResult(i,REQUEST_CODE)
     }
 }
